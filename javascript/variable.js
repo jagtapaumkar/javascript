5547527ff3643b3565059d55bf404ae53b5f8c5f
jagtapaumkar <148558244+jagtapaumkar@users.noreply.github.com>
2024-11-28 18:51:13 +0000

    testing the variables in JS

---
+++ javascript/variable.js
@@ -0,0 +1,10 @@
+const productPrice="50090" 
+let  productSale="done"
+var productRange="50"
+
+
+productRange="8891"
+productSale="notdone"
+let productstatus;
+console.log(productRange);
+console.table([productPrice,productRange,productSale,productstatus])
