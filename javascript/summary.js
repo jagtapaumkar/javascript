1c6c4f8eb65f14236b53cf1df6c7088629872362
jagtapaumkar <148558244+jagtapaumkar@users.noreply.github.com>
2024-11-29 14:56:20 +0000

    Summary of datatypes in js

---
+++ javascript/summary.js
@@ -0,0 +1,19 @@
+// Primitive 
+
+//7 types 
+// string 
+// Boolean
+// number 
+// null 
+// Symbol
+// undefined 
+// BigInt
+
+const score=100 
+const scoreValue= 100.98
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+//reference (non primitive)
+//array objects function 
