5a4cf39c480ef880e0aa0fb96443d804d9c26d89
jagtapaumkar <148558244+jagtapaumkar@users.noreply.github.com>
2024-11-29 14:56:06 +0000

    operation in JS

---
+++ javascript/operation.js
@@ -0,0 +1,20 @@
+// let someNumber =33
+
+// let stringNumber =String(someNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+
+/***********Operation**********/
+
+let num= 33
+let negnum = -num
+console.log(negnum);
+
+
+console.log(3+3);
+console.log(6-3);
+console.log(2*3);
+console.log(3**3);
+console.log(69/3);
+console.log(68%3);
